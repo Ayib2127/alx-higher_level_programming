4e3fc38a1ca149e69b4ef0b2cc4326cd86945883
Yibeltal Ebabu <yibeltal2127@gmail.com>
2022-08-25 17:18:17 -0700

    Final Commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
-    print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
-    for i, s in enumerate(argv):
-        if i > 0:
-            print("{:d}: {:s}".format(i, s))
+if __name__ == '__main__':
+    import sys
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print("{}: {}".format(i, sys.argv[i]))
