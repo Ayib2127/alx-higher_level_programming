2df10638a39c4314bdb0746ee3dc2af440fd985b
Yibeltal Ebabu <yibeltal2127@gmail.com>
2022-09-02 06:57:07 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), elm)) for elm in matrix]
+    """ Compute a new matrix with each element squared
+    """
+    if matrix is not None:
+        return list(map(
+            lambda row: list(map(
+                lambda x: x ** 2, row
+            )),
+            matrix
+        ))
+    return None
